d71bfb4ce76f2b423871d7a23f0f412b5d48ccb8
Dilton Neves <diltonfilho@multiplix.net.br>
2019-06-03 16:31:26 -0300

    my first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+//Get all elements from menu
+
+const allItems = document.querySelectorAll(".navmenu ul li a");
+
+allItems.forEach(item => {
+  item.addEventListener("click", function (e) {
+    for (var i = 0; i < allItems.length; i++) {
+      allItems[i].classList.remove("active");
+    }
+    this.classList.add("active");
+  });
+});
